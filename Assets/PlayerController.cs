f97f375fe3de44f1ab6fbf0b20c384da8970d114
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-08-28 21:46:10 +0200

    Initial Commit
    World made with Lovecraft <3

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Quaternion rotationspeed;
+    public float playerSpeed = 20f;
+	// Use this for initialization
+	void Start () {
+        //
+        //rotationspeed = Vector3.left;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /* if (Input.GetKey("up")) {
+             transform.position += Vector3.forward * Time.deltaTime; 
+         }
+         if (Input.GetKey("down")) {
+             transform.position += Vector3.back * Time.deltaTime;
+         }
+         if (Input.GetKey("left"))
+         {
+             transform.rotation = transform.rotation * Vector3.left * Time.deltaTime;
+         }
+         if (Input.GetKey("right")) {
+
+         }*/
+        if (Input.GetKeyDown("w")) {
+
+           transform.position += playerSpeed * Time.deltaTime * Vector3.up;
+        }else if (Input.GetKeyDown("s")){
+            transform.position += playerSpeed * Time.deltaTime * Vector3.down;
+        }
+    }
+}
